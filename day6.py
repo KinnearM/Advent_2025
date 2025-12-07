23f4796c94a8573e0b9852bdb9ae72d3e8d90b3b
Meryl Kinnear <79854376+KinnearM@users.noreply.github.com>
2025-12-07 01:32:00 +0000

    Implement split_into_char_columns and part_2 functions
    Added functions to split lines into character columns and calculate a total based on operations.

--- day6.py
+++ day6.py
@@ -18,6 +18,47 @@ def part_1():
             elif op == '*':
                 totals[i] *= I
     return sum(totals)
-  
-    print(part_1())
 
+def split_into_char_columns(lines):
+    max_len = max(len(line) for line in lines)
+    padded_lines = [line.ljust(max_len) for line in lines]
+
+    columns = []
+    for x in range(max_len):
+        col = [line[x] for line in padded_lines]
+        columns.append(col)
+        
+    return columns
+
+def part_2():
+
+  with open('input.txt, 'r') as f:
+        raw_data = f.read()
+    
+  lines = raw_data.splitlines()
+  cols = split_into_char_columns(lines)
+
+  sum_total=0
+  col_total=0
+  op='+'
+
+  for col in cols:
+    last_place=col.pop()
+    string=''.join(col)
+    if string.strip() == '':
+      continue
+    else:
+      value=int(string)
+      if last_place != ' ':
+        sum_total+=col_total
+        op=last_place
+        col_total=value
+      elif op=='+':
+        col_total+=value
+      elif op=='*': 
+        col_total*=value
+  sum_total += col_total
+  return sum_total
+
+print(part_1())
+print(part_2())
